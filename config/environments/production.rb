799e5d60104c81ff6b619260fdc350aaa89fe37a
Tps <you@example.com>
2017-10-03 17:56:24 +0530

    Changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.relative_url_root = "/ror/sso_client"
   config.action_controller.relative_url_root = "/ror/sso_client"
 end
