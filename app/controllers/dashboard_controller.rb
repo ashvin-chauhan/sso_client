54abb83bde67e8af89954f42e11272ce2b367f7d
bhaumikgithub <bhaumikgithub@gmail.com>
2017-10-05 18:47:23 +0530

    add partial for banner of admin and user

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,8 +7,10 @@ class DashboardController < ApplicationController
   	# @source = open("http://localhost:5000//jekyll/update/2017/09/21/post-3.html"){|f|f.read}
   	if current_user[:role] === "developer"
   		render template: "shared/developer", layout: "developer_layout"
-  	elsif current_user[:role] === "manager" || current_user[:role] == "admin"
+    elsif current_user[:role] === "manager"
       render template: "shared/manager", layout: "manager_layout"
+    elsif current_user[:role] == "admin"
+      render template: "shared/admin", layout: "manager_layout"
     end
   end
 end
